8e6e178ea0a600303009246f87055860be2b2979
Matheus M. Viana <matheusmviana@outlook.com.br>
2025-04-09 23:55:43 -0300

    Feat: Adding header logo

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -2,19 +2,21 @@ import { BadgeCheck, FileText, } from 'lucide-react';
 import NavLink from "./nav-link";
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import PlanBadge from './plan-badge';
+import Image from 'next/image';
 
 export default function Header() {
-    
+
     return (
         <nav className="container flex items-center justify-between
         py-4 lg: px - 8 px - 2 mx - auto">
             < div className="flex lg:flex-1" >
                 <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
-                    <div className='relative'>
+                    <Image src={"/SumaristaAI-logo.webp"} alt="SumaristaAI Logo" width={40} height={40} />
+                    {/* <div className='relative'>
                         <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 
                 transition-transform duration-300 ease-in-out" />
                     </div>
-                    <span className="font-extrabold lg:text-xl text-gray-900">Sumaristaai</span>
+                    <span className="font-extrabold lg:text-xl text-gray-900">Sumaristaai</span> */}
                 </NavLink>
             </div >
             <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
