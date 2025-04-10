55ad37de6d680896670c7a2757e2821f4b720c91
Matheus M. Viana <matheusmviana@outlook.com.br>
2025-04-10 00:53:38 -0300

    Feat: adding footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
         <footer className="bg-gray-50 py-12">
-            footer
+            <div className="py-12 lg:py-2 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8
+            lg:pt-12">
+                <div className="flex items-center gap-2 flex-col">
+                    <h2 className="text-2xl font-bold text-blue-400">SumaristaAI &copy; 2025</h2>
+                    <p className="text-gray-400 mt-2">Transforme conteúdos longos em resumos claros com inteligência artificial.</p>
+                </div>
+            </div>
         </footer>
     );
 }
