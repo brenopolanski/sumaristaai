c8456491372ec064ff6d002aa52cb7bf0ab36647
Matheus Viana <matheusmviana@outlook.com.br>
2025-04-13 06:21:27 -0300

    feat: add custom 404 error page with gradient background

---
+++ src/app/not-found.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+import BgGradient from "@/components/common/bg-gradient";
+import { MotionDiv } from "@/components/common/motion-wrapper";
+import { itemVariants } from "@/utils/constants";
+import { Badge } from "@/components/ui/badge";
+
+const Custom404: React.FC = () => {
+  return (
+    <>
+      <BgGradient />
+      <div className="flex items-center justify-center h-[70vh] flex-col">
+        <MotionDiv
+          variants={itemVariants}
+          className="relative p-[1px] overflow-hidden rounded-full bg-linear-to-r from-blue-200 to-blue-800 animate-gradient-x group"
+        >
+          <Badge
+            variant={"secondary"}
+            className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors duration-200"
+          >
+            <p className="text-blue-600">Erro 404</p>
+          </Badge>
+        </MotionDiv>
+        <p className="font-bold py-6 text-center text-4xl sm:text-5xl lg:text-6xl flex flex-wrap justify-center gap-x-2 gap-y-3">
+          Oops! Página não encontrada.
+        </p>
+        <p>
+          <Link
+            href="/"
+            className="text-blue-500 text-lg font-medium underline mt-8"
+            aria-label="Voltar para a página inicial"
+          >
+            Voltar para a página inicial
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Custom404;
