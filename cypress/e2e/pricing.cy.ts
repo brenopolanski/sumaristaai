0db942ddbe02e1c46b65c94b0d1c89cfce3fdda1
Matheus Viana <matheusmviana@outlook.com.br>
2025-05-07 14:09:01 -0300

    chore: update CI/CD configuration and Cypress tests

--- cypress/e2e/pricing.cy.ts
+++ cypress/e2e/pricing.cy.ts
@@ -9,17 +9,16 @@ describe("Seção de Preços", () => {
 
     cy.contains("Básico").should("exist");
     cy.contains("Para usuários ocasional").should("exist");
-    cy.contains("R$ 19").should("exist");
+    cy.contains("R$ 5").should("exist");
     cy.contains("BRL").should("exist");
     cy.contains("/mês").should("exist");
 
-    cy.contains("5 sumários por mês").should("exist");
     cy.contains("Processamento padrão").should("exist");
     cy.contains("Suporte por email").should("exist");
 
     cy.contains("Pro").should("exist");
     cy.contains("Para profissionais e equipes").should("exist");
-    cy.contains("R$ 89").should("exist");
+    cy.contains("R$ 14,9").should("exist");
     cy.contains("BRL").should("exist");
     cy.contains("/mês").should("exist");
 
