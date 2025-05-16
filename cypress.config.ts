1aac2f806c57eeacda223d4693263173b01e26ea
Matheus Viana <matheusmviana@outlook.com.br>
2025-05-15 22:55:51 -0300

    chore: add baseUrl to e2e configuration in Cypress setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "m9rhg4",
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {},
   },
   component: {
